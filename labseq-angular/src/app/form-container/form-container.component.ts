05fda384643b2e34784f1da731a060b913edd76d
kaleberibeiro <kaleberibeiro@ua.pt>
2025-01-26 12:06:10 +0000

    second stage

--- labseq-angular/src/app/form-container/form-container.component.ts
+++ labseq-angular/src/app/form-container/form-container.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ResultLabseqComponent } from "../result-labseq/result-labseq.component";
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { LabseqServiceService } from '../labseq-service.service';
 
 @Component({
   selector: 'app-form-container',
-  imports: [FormsModule, ResultLabseqComponent, NgIf],
+  imports: [FormsModule, ResultLabseqComponent, NgIf, NgClass],
   templateUrl: './form-container.component.html',
   styleUrl: './form-container.component.css'
 })
@@ -15,17 +15,20 @@ export class FormContainerComponent {
   n = 0
   result = 0
   hasValue = false
+  error = ''
 
   constructor(private labseqServ: LabseqServiceService) {}
 
   getLabSeqNum(): void {
     this.labseqServ.getLabSeqNum(this.n).subscribe({
       next: (response) => {
+        this.error = ''
         this.hasValue = true
         this.result = response
       },
       error: (error) => {
-        console.error(error);
+        console.error(error.error.error);
+        this.error = error.error.error;
         this.hasValue = false
       }
     })
--- labseq/src/main/java/org/acme/Resources/LabSeqResource.java
+++ labseq/src/main/java/org/acme/Resources/LabSeqResource.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.acme.Services.LabSeqService;
 
@@ -26,7 +27,10 @@ public class LabSeqResource {
     @Produces("application/json")
     public Response getLabSeq(@PathParam("n") int n) {
         if(n < 0) {
-            return Response.status(Response.Status.BAD_REQUEST).build();
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\": \"Input must be a non-negative integer.\"}")
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
         } else {
             return Response.ok(labSeqServ.labSeqCalc(n)).build();
         }
